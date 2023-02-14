d1ea9cfa70231c18c6017f60f7f8b4d4d34f355b
marcello <marcello.rossi@techedgegroup.com>
2023-02-14 22:54:57 +0100

    text changes

--- src/Pages/DocumentDrive/DocumentDrive.tsx
+++ src/Pages/DocumentDrive/DocumentDrive.tsx
@@ -4,6 +4,8 @@ import MdIntro from './1-DocumentDrive-Intro.md';
 import MdBody from './2-DocumentDrive-Body.md';
 import { Kpi } from "../../Components/Kpi/Kpi";
 import { Typography } from "../../Components/Typography/Typography";
+import { Gantt, Task, ViewMode } from "gantt-task-react";
+import "gantt-task-react/dist/index.css";
 
 // gantt chart
 // https://www.npmjs.com/package/gantt-task-react
@@ -33,6 +35,7 @@ export const DocumentDrive = () => {
                 components={[
                     MdIntro,
                     <DocumentDriveKpis />,
+                    // <DocumentDriveGantt />,
                     MdBody
                 ]}/>
 }
@@ -64,4 +67,30 @@ export const DocumentDriveKpis = () => {
 
             </div>
         </div>
+}
+
+export const DocumentDriveGantt = () => {
+    let tasks: Task[] = [
+        {
+          start: new Date(2020, 10, 1),
+          end: new Date(2021, 1, 31),
+          name: 'Ideation & Discovery',
+          id: 'Task 0',
+          type:'task',
+          progress: 100,
+          isDisabled: true,
+          styles: { progressColor: '#313131', progressSelectedColor: '#313131' },
+        },
+        {
+          start: new Date(2020, 11, 1),
+          end: new Date(2021, 2, 27),
+          name: 'Technical Project Preparation',
+          id: 'Task 1',
+          type:'task',
+          progress: 100,
+          isDisabled: true,
+          styles: { progressColor: '#313131', progressSelectedColor: '#313131' },
+        }
+    ];
+    return <Gantt tasks={tasks} viewMode={ViewMode.Month} listCellWidth=""/>
 }
