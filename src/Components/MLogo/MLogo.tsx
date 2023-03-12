51ee7c4c8bbd481319edf89cf7b03f7879e4a76a
marcello <marcello.rossi@techedgegroup.com>
2023-03-12 15:36:59 +0100

    m logo

--- src/Components/MLogo/MLogo.tsx
+++ src/Components/MLogo/MLogo.tsx
@@ -8,13 +8,16 @@ import MLogoSvg from "./../../Images/MLogo.svg";
 
 export const MLogo = () => {
     return <svg width="307" height="202" viewBox="0 0 307 202" fill="none" xmlns="http://www.w3.org/2000/svg" className="logo-svg">
-    <path d="M101.705 201V1H0.300904V46.24H61.9862V201H101.705Z" className="logo-fill"/>
-    <path d="M0 1H0.300904M0.300904 1H101.705V201H61.9862V46.24H0.300904V1Z" className="logo-software" stroke-width="2"/>
-    <path d="M203.411 201V1H102.006V46.24H163.692V201H203.411Z" className="logo-fill"/>
-    <path d="M101.705 1H102.006M102.006 1H203.411V201H163.692V46.24H102.006V1Z" className="logo-agile" stroke-width="2"/>
-    <path d="M305.116 201V1H203.712V46.24H265.397V201H305.116Z"  className="logo-fill"/>
-    <path d="M203.411 1H203.712M203.712 1H305.116V201H265.397V46.24H203.712V1Z" className="logo-design"  stroke-width="2"/>
+    <path d="M305.705 201V1H204.301L204.301 41L265.986 41L265.986 201H305.705Z" className="logo-fill"/>
+    <path d="M204 1H204.301M204.301 1H305.705V201H265.986L265.986 41L204.301 41L204.301 1Z" className="logo-software" stroke-width="2"/>
+    <path d="M203.705 201V1H102.301L102.301 41L163.986 41L163.986 201H203.705Z" className="logo-fill"/>
+    <path d="M102 1H102.301M102.301 1H203.705V201H163.986L163.986 41L102.301 41L102.301 1Z" className="logo-agile" stroke-width="2"/>
+    <path d="M101.705 201V1H0.300904L0.300904 41L61.9861 41L61.9861 201H101.705Z" className="logo-fill"/>
+    <path d="M0 1H0.300904M0.300904 1H101.705V201H61.9861L61.9861 41L0.300904 41L0.300904 1Z" className="logo-design" stroke-width="2"/>
     </svg>
     
     
 }
+
+
+
