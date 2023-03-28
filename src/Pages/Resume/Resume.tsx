b6142c47d718bf1ca998865bff9555f5a4cc7391
marcello <marcello.rossi@techedgegroup.com>
2023-03-28 13:24:41 +0200

    CV fichissimo

--- src/Pages/Resume/Resume.tsx
+++ src/Pages/Resume/Resume.tsx
@@ -1,15 +1,16 @@
 import "./Resume.scss";
 import CurriculumVitaePdf from "./../../Resources/Curriculum Vitae - Marcello Rossi.pdf";
-import CurriculumVitaeImg from "./../../Images/Curriculum Vitae - Marcello Rossi.png";
+import CurriculumVitaeSvg from "./../../Resources/Curriculum Vitae - Marcello Rossi.svg";
 
 export const Resume = () => {
     return  <div className={`animate__animated animate__fadeIn page-content resume`}>
                 <div className="container">
-                    <img src={CurriculumVitaeImg} />
+                    <img src={CurriculumVitaeSvg} />
+                    {/* <iframe src={CurriculumVitaePdf} /> */}
                 </div>
                 <div className="download-area">
                     <a className={`w3-button w3-large w3-mr-black w3-round w3-mr-lifted`}
-                        href={CurriculumVitaePdf} target="_blank" download> 
+                        href={CurriculumVitaePdf} target="_blank" download="Curriculum Vitae - Marcello Rossi.pdf"> 
                         Download Resume
                     </a>
                 </div>
