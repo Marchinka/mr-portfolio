6a29ccbe205b160caacf659e790e738507794fb3
marcello <marcello.rossi@techedgegroup.com>
2023-03-28 11:53:47 +0200

    pdf page and download

--- src/Pages/Resume/Resume.tsx
+++ src/Pages/Resume/Resume.tsx
@@ -1,9 +1,18 @@
 import "./Resume.scss";
-import CurriculumVitae from "./../../Images/Curriculum Vitae - Marcello Rossi.pdf";
+import CurriculumVitaePdf from "./../../Resources/Curriculum Vitae - Marcello Rossi.pdf";
+import CurriculumVitaeImg from "./../../Images/Curriculum Vitae - Marcello Rossi.png";
 
 export const Resume = () => {
     return  <div className={`animate__animated animate__fadeIn page-content resume`}>
-                <iframe src={CurriculumVitae} frameBorder={"0"}></iframe>
+                <div className="container">
+                    <img src={CurriculumVitaeImg} />
+                </div>
+                <div className="download-area">
+                    <a className={`w3-button w3-large w3-mr-black w3-round w3-mr-lifted`}
+                        href={CurriculumVitaePdf} target="_blank" download> 
+                        Download Resume
+                    </a>
+                </div>
             </div>
 }
 
