250d7fd4af66cc874d5561a5cf77e89cfab3eab2
marcello <marcello.rossi@techedgegroup.com>
2023-03-30 07:01:51 +0200

    loader

--- src/Pages/Resume/Resume.tsx
+++ src/Pages/Resume/Resume.tsx
@@ -1,18 +1,28 @@
 import "./Resume.scss";
 import CurriculumVitaePdf from "./../../Resources/Curriculum Vitae - Marcello Rossi.pdf";
 import CurriculumVitaeSvg from "./../../Resources/Curriculum Vitae - Marcello Rossi.svg";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faSpinner  } from "@fortawesome/free-solid-svg-icons";
 
 export const Resume = () => {
+    const [loaded, setLoaded] = useState(false);
+    
     return  <div className={`animate__animated animate__fadeIn page-content resume`}>
-                <div className="container">
-                    <img src={CurriculumVitaeSvg} />
-                    {/* <iframe src={CurriculumVitaePdf} /> */}
-                </div>
-                <div className="download-area">
-                    <a className={`w3-button w3-large w3-mr-black w3-round w3-mr-lifted`}
-                        href={CurriculumVitaePdf} target="_blank" download="Curriculum Vitae - Marcello Rossi.pdf"> 
-                        Download Resume
-                    </a>
+                {!loaded && <div className="loader">
+                    <FontAwesomeIcon icon={faSpinner} spin={true} size="6x" className="text muted"/>
+                </div>}
+                <div style={{display: loaded ? "block" : "none"}}>
+                    <div className="container">
+                        <img src={CurriculumVitaeSvg} onLoad={e => setLoaded(true)}/>
+                        {/* <iframe src={CurriculumVitaePdf} /> */}
+                    </div>
+                    <div className="download-area">
+                        <a className={`w3-button w3-large w3-mr-black w3-round w3-mr-lifted`}
+                            href={CurriculumVitaePdf} target="_blank" download="Curriculum Vitae - Marcello Rossi.pdf"> 
+                            Download Resume
+                        </a>
+                    </div>
                 </div>
             </div>
 }
